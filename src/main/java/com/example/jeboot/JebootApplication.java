979b20e764e4789612e429df757d645872c896c6
“用hua-1 <hn121907@163.com>
2019-01-08 19:19:39 +0800

    配置文件

--- src/main/java/com/example/jeboot/JebootApplication.java
+++ src/main/java/com/example/jeboot/JebootApplication.java
@@ -1,9 +1,11 @@
 package com.example.jeboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.jeboot.dao")
 public class JebootApplication {
 
     public static void main(String[] args) {
