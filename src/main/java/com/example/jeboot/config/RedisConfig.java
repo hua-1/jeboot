f4a4619c58685be8a5af8002ba05b0e544f2ea53
lianghuating <lianghuating@baoxiucai.com>
2019-01-12 17:37:18 +0800

    redis

---
+++ src/main/java/com/example/jeboot/config/RedisConfig.java
@@ -0,0 +1,19 @@
+package com.example.jeboot.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+@Configuration
+public class RedisConfig {
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(){
+        StringRedisTemplate redisTemplate = new StringRedisTemplate(redisConnectionFactory);
+        return redisTemplate;
+    }
+}
